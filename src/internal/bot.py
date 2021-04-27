7785f2dc4d7f5a1650635bd973b0085293e88069
vcokltfre <vcokltfre@gmail.com>
2021-04-27 09:31:02 +0100

    feat: initial content

---
+++ src/internal/bot.py
@@ -0,0 +1,77 @@
+from discord.ext import commands
+from discord import Intents, Message, AllowedMentions
+
+from loguru import logger
+from traceback import format_exc
+from aiohttp import ClientSession
+
+from .context import Context
+
+
+class Bot(commands.Bot):
+    """A subclass of commands.Bot with additional functionality."""
+
+    def __init__(self, *args, **kwargs):
+        logger.info("Starting up...")
+
+        intents = Intents.all()
+
+        super().__init__(
+            command_prefix=self.get_prefix,
+            intents=intents,
+            help_command=None,
+            allowed_mentions=AllowedMentions(roles=False, everyone=False),
+            *args,
+            **kwargs
+        )
+
+        self.session: ClientSession = None
+
+    def add_cog(self, cog) -> None:
+        """Add a cog to the bot. Does not add disabled cogs."""
+
+        if not hasattr(cog, "enabled") or cog.enabled:
+            logger.info(f"Loading cog {cog.qualified_name}")
+            return super().add_cog(cog)
+        logger.info(f"Not loading cog {cog.qualified_name}")
+
+    def load_extensions(self, *exts) -> None:
+        """Load a given set of extensions."""
+
+        logger.info(f"Starting loading {len(exts)} extensions...")
+
+        success = 0
+
+        for ext in exts:
+            try:
+                self.load_extension(ext)
+            except Exception as e:
+                logger.error(f"Error while loading {ext}: {e}:\n{format_exc()}")
+            else:
+                logger.info(f"Successfully loaded extension {ext}.")
+                success += 1
+
+        logger.info(f"Extension loading finished. Success: {success}. Failed: {len(exts) - success}.")
+
+    async def login(self, *args, **kwargs) -> None:
+        """Create the ClientSession before login."""
+        logger.info("Logging in to Discord...")
+
+        self.session = ClientSession()
+
+        await super().login(*args, **kwargs)
+
+    async def get_prefix(self, message: Message):
+        """Get a dynamic prefix."""
+
+        return "!"
+
+    async def get_context(self, message: Message):
+        """Get the context with the custom context class."""
+
+        return await super().get_context(message, cls=Context)
+
+    async def on_connect(self):
+        """Log the connect event."""
+
+        logger.info("Connected to Discord.")
